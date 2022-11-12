d146e755a07458b34f7086bbcdb2ecd997347932
EsvinGonzalez <106945397+EsvinGonzalez@users.noreply.github.com>
2022-11-12 11:12:09 -0600

    Examen
    Examen

---
+++ Empleados/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    class Empleados
+    {
+        private float[] turnoMañana;
+        private float[] turnoTarde;
+
+        public void Cargar() 
+        {
+            string linea;
+            turnoMañana=new float[4];
+            turnoTarde=new float[4];
+            Console.WriteLine("Sueldo de empleados del turno por la mañana:");
+            for(int f = 0; f < 4; f++) 
+            {
+                Console.Write("Ingrese sueldo:  Q.");
+                linea = Console.ReadLine();              
+                turnoMañana[f]=float.Parse(linea);
+            }
+            Console.WriteLine("Sueldo de empleados del turno por la tarde:");
+            for(int f = 0; f < 4; f++) 
+            {
+                Console.Write("Ingrese sueldo: Q.");
+                linea = Console.ReadLine();              
+                turnoTarde[f]=float.Parse(linea);
+            }
+        }
+
+        public void CalcularGastostotales() 
+        {
+            float mañana=0;
+            float tarde=0;
+            for(int f = 0; f < 4; f++)
+            {
+                mañana=mañana+turnoMañana[f];
+                tarde=tarde+turnoTarde[f];
+            }
+            Console.WriteLine("Gastos del turno de la mañana:  Q."+mañana);
+            Console.WriteLine("Gastos del turno de la tarde:  Q."+tarde );
+            Console.ReadKey();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Empleados pv = new Empleados();
+            pv.Cargar();
+            pv.CalcularGastostotales();
+        }
+    }
+}
---
+++ Matriz3x5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejercicio4
+{
+    class Matriz_3x5
+    {
+        private int[,] mat;
+
+        public void Ingresar()
+        {
+            mat = new int[3, 5];
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write("Ingrese valor de  posicion ["+(f+1)+","+(c+1)+"]: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    mat[f, c] = int.Parse(linea);
+                }
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write(mat[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Matriz_3x5 ma = new Matriz_3x5();
+            ma.Ingresar();
+            ma.Imprimir();
+        }
+    }
+}
